36068326083e0705e6112ca37639a9e696fffd39
Edward Koome <koomeedward77@gmail.com>
2020-08-05 19:41:14 +0300

    add pizza options

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+document.querySelector('')
+    const SmallSize = document.querySelector("#small");
+    const mediumSize = document.querySelector("#medium");
+    const mediumSize = document.querySelector("#large");
+
+
+
+function pizzaSize() {
+    
+}
